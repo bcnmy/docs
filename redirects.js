71d0eb4251c946bc35866022ec008e070f315460
Yash Garg <yashygkf@gmail.com>
2023-08-03 17:51:29 +0530

    fix: naming corrections

--- redirects.js
+++ redirects.js
@@ -3,44 +3,44 @@ const redirectLinks= [
     to: '/docs/category/smart-accounts',
     from: '/sdk-reference/smart-account-methods',
   },{
-    to: '/sdk-reference/sending-transactions/gasless-transactions',
-    from: '/docs/1.0/category/gasless-transactions-3',
+    from: '/sdk-reference/sending-transactions/gasless-transactions',
+    to: '/docs/1.0/category/gasless-transactions-3',
   },
   {
-    to: '/quick-start/build-your-first-aa-powered-dapp/creating-a-gasless-transaction',
-    from: '/docs/1.0/quickstart/gasless-txn',
+    from: '/quick-start/build-your-first-aa-powered-dapp/creating-a-gasless-transaction',
+    to: '/docs/1.0/quickstart/gasless-txn',
   },
   {
-    to: '/guides/sdk-migration-guide',
-    from: '/docs/1.0/guides/sdk-migration',
+    from: '/guides/sdk-migration-guide',
+    to: '/docs/1.0/guides/sdk-migration',
   },
   {
-    to: '/docs/Biconomy%20AA%20Stack/Social%20Login/setupsmartAccount',
-    from: '/docs/1.0/guides/next/smart-account-login',
+    from: '/docs/Biconomy%20AA%20Stack/Social%20Login/setupsmartAccount',
+    to: '/docs/1.0/guides/next/smart-account-login',
   },
   {
-    to: '/guides/react.js/gasless-transactions',
-    from: '/docs/1.0/category/gasless-transactions',
+    from: '/guides/react.js/gasless-transactions',
+    to: '/docs/1.0/category/gasless-transactions',
   },
   {
-    to: '/contracts/biconomy-contracts/proxy',
-    from: '/docs/1.0/biconomy-contracts/Contracts/proxy',
+    from: '/contracts/biconomy-contracts/proxy',
+    to: '/docs/1.0/biconomy-contracts/Contracts/proxy',
   },
   {
-    to: '/sdk-reference/sending-transactions/gasless-transactions/sending-erc-721-nft-tokens',
-    from: '/docs/1.0/sdk-reference/send-txn/gasless-txn/sendind-erc721',
+    from: '/sdk-reference/sending-transactions/gasless-transactions/sending-erc-721-nft-tokens',
+    to: '/docs/1.0/sdk-reference/send-txn/gasless-txn/sendind-erc721',
   },
   {
-    to: '/guides/react.js/code-examples',
-    from: '/docs/1.0/category/code-examples',
+    from: '/guides/react.js/code-examples',
+    to: '/docs/1.0/category/code-examples',
   },
   { 
-    to: '/guides/next.js/gasless-transactions/single-gasless-transaction',
-    from: '/docs/1.0/guides/next/gasless-txn/single-txn',
+    from: '/guides/next.js/gasless-transactions/single-gasless-transaction',
+    to: '/docs/1.0/guides/next/gasless-txn/single-txn',
   },
   { 
-    to: '/products/enable-gasless-transactions/networks-supported',
-    from: '/docs/1.0/sdk-reference/network-supported',
+    from: '/products/enable-gasless-transactions/networks-supported',
+    to: '/docs/1.0/sdk-reference/network-supported',
   },
 ];
 
